32f7924023f525ee9d7568174d023acde32333c7
Lloyd Culpepper <lloydculpepper4@gmail.com>
2022-02-26 19:27:12 +0000

    basic routes added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +20,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('login')->uses([LoginController::class, 'index'])->name('login.index');
+Route::put('login')->uses([LoginController::class, 'store'])->name('login.store');
+Route::put('logout')->uses([LoginController::class, 'delete'])->name('login.delete');
+
+Route::get('register')->uses([RegisterController::class, 'create'])->name('register.create');
+Route::put('register')->uses([RegisterController::class, 'store'])->name('register.store');
+
 Route::group(['prefix' => 'recipe'], function (): void {
-    Route::get('/')->uses([RecipeController::class, 'get'])->name('recipe.index');
-    Route::get('{recipe}')->uses('RecipeController@get')->name('recipe.get');
-    Route::get('create')->uses('RecipeController@create')->name('recipe.create');
-    Route::delete('delete')->uses('RecipeController@delete')->name('recipe.delete');
-    Route::patch('update')->uses('RecipeController@update')->name('recipe.update');
-    Route::put('store')->uses('RecipeController@store')->name('recipe.store');
+    Route::get('')->uses([RecipeController::class, 'index'])->name('recipe.index');
+    Route::get('create')->uses([RecipeController::class, 'create'])->name('recipe.create');
+    Route::get('{recipe}')->uses([RecipeController::class, 'get'])->name('recipe.get');
+    Route::delete('delete')->uses([RecipeController::class, 'delete'])->name('recipe.delete');
+    Route::patch('update')->uses([RecipeController::class, 'update'])->name('recipe.update');
+    Route::put('store')->uses([RecipeController::class, 'store'])->name('recipe.store');
 });
