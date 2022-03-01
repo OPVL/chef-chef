d598b3ba3dc7ef205b27ebb7ccd9994c487cebe9
Lloyd Culpepper <lloyd.culpepper@sophos.com>
2022-03-01 15:50:55 +0000

    create recipe test

---
+++ tests/Unit/Action/CreateRecipeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Action;
+
+use App\Actions\CreateRecipe;
+use App\Actions\SortIngredientsByLocation;
+use App\Models\Cuisine;
+use App\Models\Ingredient;
+use App\Models\Recipe;
+use App\Models\StorageLocation;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateRecipeTest extends TestCase
+{
+    use DatabaseMigrations, WithFaker;
+
+    /** @test */
+    public function can_create_recipe(): void
+    {
+        $cuisine = Cuisine::factory()->create();
+        $name = $this->faker->word();
+        $description = $this->faker->sentences(3, true);
+
+        $recipe = $this->action()->execute([
+            'name' => $name,
+            'description' => $description,
+            'cuisine_id' => $cuisine->id,
+        ]);
+
+        $this->assertModelExists($recipe);
+        $this->assertEquals($name, $recipe->name);
+        $this->assertEquals($description, $recipe->description);
+        $this->assertEquals($cuisine->id, $recipe->cuisine_id);
+    }
+
+    protected function action(): CreateRecipe
+    {
+        return app(CreateRecipe::class);
+    }
+}
