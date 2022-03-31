e420a8359ff4e234ab3eebf26085955976606bc7
Lloyd Culpepper <lloydculpepper4@gmail.com>
2022-03-31 23:34:47 +0100

    added forgot, styled login & placeholders

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginUser;
 use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    use WithFaker;
+
     public function index(): View
     {
-        return view('auth.login');
+        return view('auth.login', [
+            'emailPlaceholder' => 'jane.doe@example.com',
+            'passwordPlaceholder' => 'chained-to-roberto',
+        ]);
     }
 
     public function store(LoginUser $request): RedirectResponse
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,36 @@
         login
     @endslot
     @section('content')
-        <form action="{{ route('login.store') }}" method="post">
-            @method('PUT')
-            @csrf
+        <div class="hero">
+            <h4 class="page-title">login</h4>
 
-            <label for="email">email</label>
-            <input type="text" name="email" id="email" autocomplete="email">
+            <form action="{{ route('login.store') }}" method="post">
+                @method('PUT')
+                @csrf
 
-            <label for="password">password</label>
-            <input type="password" name="password" id="password" autocomplete="password">
+                <div class="input-group">
+                    @error('email')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <label for="email">email</label>
+                    <input type="email" name="email" id="email" autocomplete="email" required
+                        placeholder="{{ $emailPlaceholder }}">
+                </div>
 
-            <button type="submit">login</button>
-        </form>
+                <div class="input-group">
+                    @error('password')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <label for="password">password</label>
+                    <input type="password" name="password" id="password" autocomplete="password" required
+                        placeholder="{{ $passwordPlaceholder }}">
+                </div>
+
+                <button type="submit" class="btn main submit">login</button>
+
+                <a class="not-focus-link" href="{{ route('forgot') }}">forgotten password?</a>
+                <a class="not-focus-link" href="{{ route('register') }}">don't have an account?</a>
+            </form>
+        </div>
     @endsection
 </x-layout.standard>
