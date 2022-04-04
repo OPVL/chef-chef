461f48ca44958c926fe0105365b5cb78eacb10ee
Lloyd Culpepper <lloydculpepper4@gmail.com>
2022-04-05 00:32:00 +0100

    seed function for nonce

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function seedDebugNonce(string ...$data): string
+    {
+
+        $data = implode(':', collect($data)->map(function ($seed) {
+            try {
+                return str($seed);
+            } catch (\Throwable $th) {
+                return '';
+            }
+        })->toArray());
+        dump($data);
+        $hash = hash('sha256', $data);
+        dump($hash);
+        session(['debug_nonce' => $hash]);
+
+        return $hash;
+    }
 }
